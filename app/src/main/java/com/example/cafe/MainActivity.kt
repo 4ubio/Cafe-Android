1b54a12323160cc79a0bfb9b5c88e9dca510dd82
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-02-14 16:15:03 -0600

    Add Login to First Screen

--- app/src/main/java/com/example/cafe/MainActivity.kt
+++ app/src/main/java/com/example/cafe/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cafe.ui.theme.CafeTheme
+import com.example.cafe.views.login_screen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    login_screen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     CafeTheme {
-        Greeting("Android")
+        login_screen()
     }
 }
