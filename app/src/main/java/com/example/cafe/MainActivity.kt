1287e425532403c4ed3c08994e7e206f39b0b5dd
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-05-10 23:31:30 -0600

    Fix String & Gadle bug

--- app/src/main/java/com/example/cafe/MainActivity.kt
+++ app/src/main/java/com/example/cafe/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cafe
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +15,7 @@ import com.example.cafe.handlers.NavManager
 import com.example.cafe.ui.theme.CafeTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,6 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
